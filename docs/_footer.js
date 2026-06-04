9fbd8d6f230871d673c9df5e4b067d04d1455de6
Dierk Koenig <dierk.koenig@canoo.com>
2026-06-04 15:23:21 +0200

    refactor: move footer in central file

---
+++ docs/_footer.js
@@ -0,0 +1,18 @@
+
+const footer = `<footer>
+     <h2>Contact</h2> <div></div>
+     <address>
+             Prof. Dierk König
+             University of Applied Sciences and Arts
+             Northwestern Switzerland
+             School of Computer Science
+             Bahnhofstrasse 6
+             CH-5210 Windisch
+     
+             <a href="mailto:dierk.koenig@fhnw.ch">dierk.koenig@fhnw.ch</a>
+             <a href="https://www.fhnw.ch/en/people/dierk-koenig">Personal Page FHNW</a>
+             
+     </address>
+     </footer>
+`;
+document.getElementById("footer").innerHTML = footer;
