8f35826763b2ecbe39b79c301c5b07fbd33bd538
Dierk Koenig <dierk.koenig@canoo.com>
2026-06-06 00:15:43 +0200

    feature: introduce navigation and view-transition

---
+++ docs/nav.js
@@ -0,0 +1,10 @@
+document.getElementById("nav").innerHTML = `
+    <ul>
+        <li><a href="AiPerfectStorm.html">AI Fallacies: Perfect Storm</a></li>
+        <ol>
+            <li><a href="AiSugarHigh.html">Sugar High</a></li>
+        </ol>
+        <li><a href="AiDoesNotWork.html">AI Does not Work</a></li>
+        <li><a href="ComputerScienceEducation.html">Computer Science Education</a></li>
+    </ul>
+`;
