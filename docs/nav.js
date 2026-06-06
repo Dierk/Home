9252fa0c0d72b23dfcbad3e5823134314d89716d
Dierk Koenig <dierk.koenig@canoo.com>
2026-06-06 22:24:54 +0200

    add: AiNoCopilot.html

--- docs/nav.js
+++ docs/nav.js
@@ -4,6 +4,7 @@ document.getElementById("nav").innerHTML = `
         <ol>
             <li><a href="AiSugarHigh.html">Sugar High</a></li>
             <li><a href="AiConditioning.html">Conditioning</a></li>
+            <li><a href="AiNoCopilot.html">No Copilot</a></li>
         </ol>
         <li><a href="AiDoesNotWork.html">AI Does not Work</a></li>
         <li><a href="ComputerScienceEducation.html">Computer Science Education</a></li>
